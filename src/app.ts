2946641cbe500cefaaf13601cb140df3a8f6cb93
김대웅 <dkssud9556@dsm.hs.kr>
2020-08-13 13:55:35 +0900

    [ADD] application

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import "reflect-metadata";
+import * as express from "express";
+import init from "./loaders";
+import config from "./config";
+import logger from "./loaders/logger";
+
+async function startServer() {
+  const app = express();
+
+  await init(app);
+
+  app.listen(config.port, (err) => {
+    if (err) {
+      logger.error(err);
+      process.exit(1);
+      return;
+    }
+    logger.debug(`Server listening on ${config.port}`);
+  });
+}
+
+startServer();
