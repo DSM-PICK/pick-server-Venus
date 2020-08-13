319947dc9d8908c8c96bf7052cf543840232d19e
김대웅 <dkssud9556@dsm.hs.kr>
2020-08-13 13:56:03 +0900

    [ADD] errors

---
+++ src/errors.ts
@@ -0,0 +1,19 @@
+class HttpError extends Error {
+  public status: number;
+  public code: string;
+  constructor(message: string, status: number, code: string) {
+    super(message);
+    this.status = status;
+    this.code = code;
+  }
+}
+
+const invalidLoginInformationError = new HttpError("Invalid id or pw", 403, "");
+const invalidParameterError = new HttpError("Invalid parameter", 400, "");
+const apiNotFoundError = new HttpError("API not found", 404, "");
+
+export {
+  invalidLoginInformationError,
+  invalidParameterError,
+  apiNotFoundError,
+};
