2073911a8b17d2a4a6d99f0a7b3b71cf92872327
김대웅 <dkssud9556@dsm.hs.kr>
2020-08-16 02:27:51 +0900

    [DELETE] useless model and dependency injector

--- src/loaders/dependencyInjector.ts
+++
@@ -1,9 +0,0 @@
-import { Container } from "typedi";
-import logger from "./logger";
-
-export default ({ repositories }: { repositories: object }) => {
-  Object.keys(repositories).forEach((key) =>
-    Container.set(key, new repositories[key]())
-  );
-  Container.set("logger", logger);
-};
