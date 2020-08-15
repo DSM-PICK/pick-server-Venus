9cff9d22bff469e0f12e599fafdca9d0065b22df
김대웅 <dkssud9556@dsm.hs.kr>
2020-08-16 02:31:51 +0900

    [ADD] interfaces

---
+++ src/interfaces/index.ts
@@ -0,0 +1,5 @@
+import IAdminRepository from "./adminRepositoryInterface";
+import ILogger from "./loggerInterface";
+import IAdmin from "./adminInterface";
+
+export { IAdminRepository, IAdmin, ILogger };
