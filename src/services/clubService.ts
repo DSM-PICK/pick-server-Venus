088b3d4039f1d6bd6fea306f261be7a51933cf91
김대웅 <dkssud9556@dsm.hs.kr>
2020-08-20 17:06:12 +0900

    [ADD] add club service

--- src/services/clubService.ts
+++ src/services/clubService.ts
@@ -1,12 +1,27 @@
-import { ILogger, IClubRepository } from "../interfaces";
+import { ILogger, IClubRepository, IClub } from "../interfaces";
+import { Club } from "../models";
+import IClubLocationRepository from "../interfaces/IClubLocationRepository";
+import { invalidParameterError } from "../errors";
 
 export default class ClubService {
   constructor(
     private clubRepository: IClubRepository,
+    private clubLocationRepository: IClubLocationRepository,
     private logger: ILogger
   ) {}
 
-  public getClubs() {
+  public getClubs(): Promise<Club[]> {
     return this.clubRepository.findAll();
+  }
+
+  public async addClub(club: IClub): Promise<Club> {
+    if (
+      (await this.clubLocationRepository.isNotExistLocation(club.location)) ||
+      (await this.clubRepository.getClubByName(club.name)) ||
+      (await this.clubRepository.getClubByLocation(club.location))
+    ) {
+      throw invalidParameterError;
+    }
+    return this.clubRepository.addClub(club);
   }
 }
