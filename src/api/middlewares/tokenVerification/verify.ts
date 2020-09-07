613e18bb81a07f17fe3dcdd6921e045dbad4e5a5
김대웅 <dkssud9556@dsm.hs.kr>
2020-09-07 20:00:04 +0900

    [FIX] token verification check

--- src/api/middlewares/tokenVerification/verify.ts
+++ src/api/middlewares/tokenVerification/verify.ts
@@ -3,6 +3,9 @@ import * as jwt from "jsonwebtoken";
 import { invalidTokenError, notAccessTokenError } from "../../../errors";
 
 export default ({ token, jwtSecret }: { token: string; jwtSecret: string }) => {
+  if (!token || typeof token !== "string") {
+    throw invalidTokenError;
+  }
   const splitToken = token.split(" ");
   if (splitToken[0] !== "Bearer") {
     throw invalidTokenError;
