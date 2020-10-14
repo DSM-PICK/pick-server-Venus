7a63058c591a73d3072c7f843e4b1a991751dda7
김대웅 <dkssud9556@dsm.hs.kr>
2020-10-14 20:01:20 +0900

    [ADD] find all locations by location function to repository

--- src/interfaces/club/IClubLocationRepository.ts
+++ src/interfaces/club/IClubLocationRepository.ts
@@ -1,6 +1,7 @@
 import { ClubLocation } from "../../models";
 
-export default interface IClubRepository {
+export default interface IClubLocationRepository {
   isNotExistLocation(location: string): Promise<boolean>;
   findAll(): Promise<ClubLocation[]>;
+  findAllByLocation(location: string): Promise<ClubLocation[]>;
 }
