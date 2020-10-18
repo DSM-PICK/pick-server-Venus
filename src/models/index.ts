3a81eda208b2d36a612f6db633d46c52ddbb1de0
김대웅 <dkssud9556@dsm.hs.kr>
2020-10-18 15:37:22 +0900

    [DELETE] useless models

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,21 +1,6 @@
-import { Activity } from "./activity";
 import { Admin } from "./admin";
-import { Attendance } from "./attendance";
-import { Class } from "./class";
 import { Club } from "./club";
-import { PriorAbsence } from "./priorAbsence";
 import { Student } from "./student";
-import { Teacher } from "./teacher";
 import { ClubLocation } from "./clubLocation";
 
-export {
-  Activity,
-  Admin,
-  Attendance,
-  Class,
-  Club,
-  PriorAbsence,
-  Student,
-  Teacher,
-  ClubLocation,
-};
+export { Admin, Club, Student, ClubLocation };
