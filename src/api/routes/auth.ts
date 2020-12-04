372c31c58f45ccbc48961bf00a9f15dce899aff2
김대웅 <dkssud9556@dsm.hs.kr>
2020-12-04 09:58:07 +0900

    [ADD] tryCatchHandler to routes

--- src/api/routes/auth.ts
+++ src/api/routes/auth.ts
@@ -13,48 +13,24 @@ import config from "../../config";
 import validate, { Property } from "../middlewares/paramValidation";
 import { expiredTokenError, invalidTokenError } from "../../errors";
 import isAuth from "../middlewares/tokenVerification";
+import tryCatchHandler from "../middlewares/tryCatchHandler";
+import AuthController from "../../controllers/auth";
 
 const route = Router();
 
 export default (app: Router) => {
   app.use("/auth", route);
 
-  const adminRepository = getCustomRepository(AdminRepository);
-  const authService = new AuthService(adminRepository, config.jwtSecret);
-
   route.post(
     "/",
     validate({ schema: loginSchema, property: Property.BODY }),
-    async (req: Request, res: Response, next: NextFunction) => {
-      const admin: IAdmin = req.body;
-      try {
-        const tokens = await authService.signIn(admin);
-        return res.status(200).json(tokens);
-      } catch (e) {
-        return next(e);
-      }
-    }
+    tryCatchHandler(AuthController.signIn)
   );
 
   route.patch(
     "/",
     validate({ schema: refreshSchema, property: Property.HEADERS }),
-    (req: Request, res: Response, next: NextFunction) => {
-      const refresh_token = req.get("x-refresh-token");
-      try {
-        const accessToken = authService.tokenRefresh({
-          refresh_token,
-        });
-        res.status(200).json(accessToken);
-      } catch (e) {
-        if (e === TokenExpiredError) {
-          return next(expiredTokenError);
-        } else if (e === JsonWebTokenError) {
-          return next(invalidTokenError);
-        }
-        next(e);
-      }
-    }
+    AuthController.signUp
   );
 
   route.get("/check", isAuth, (req: Request, res: Response) => {
--- src/api/routes/club.ts
+++ src/api/routes/club.ts
@@ -1,5 +1,4 @@
-import { NextFunction, Request, Response, Router } from "express";
-import { getCustomRepository } from "typeorm";
+import { Router } from "express";
 
 import validate, { Property } from "../middlewares/paramValidation";
 import isAuth from "../middlewares/tokenVerification";
@@ -11,127 +10,40 @@ import {
   updateClubBodySchema,
   updateClubParamSchema,
 } from "../middlewares/paramValidation/schema";
-import { IClub, IPatchClubRequest, IUpdateClub } from "../../interfaces";
-import {
-  ClubLocationRepository,
-  ClubRepository,
-  NoticeRepository,
-} from "../../repositories";
-import ClubService from "../../services/clubService";
-import StudentRepository from "../../repositories/studentRepository";
-import StudentService from "../../services/studentService";
-import getNoticeEventEvmitter, {
-  clubAdd,
-  clubDelete,
-  clubInfoChange,
-  studentClubChange,
-} from "../../loaders/noticeEventEvmitter";
+import tryCatchHandler from "../middlewares/tryCatchHandler";
+import ClubController from "../../controllers/club";
 
 const route = Router();
 
 export default (app: Router) => {
   app.use("/club", route);
 
-  const clubRepository = getCustomRepository(ClubRepository);
-  const clubLocationRepository = getCustomRepository(ClubLocationRepository);
-  const studentRepository = getCustomRepository(StudentRepository);
-  const noticeRepository = getCustomRepository(NoticeRepository);
-  const clubService = new ClubService(
-    clubRepository,
-    clubLocationRepository,
-    studentRepository
-  );
-  const studentService = new StudentService(studentRepository, clubRepository);
-  const noticeEventEmitter = getNoticeEventEvmitter(noticeRepository);
-
   route.patch(
     "/students",
     isAuth,
     validate({ schema: patchClubSchema, property: Property.BODY }),
-    async (req: Request, res: Response, next: NextFunction) => {
-      const moveInfo: IPatchClubRequest = req.body;
-      const { students_num, to_club_name } = moveInfo;
-      try {
-        const students = await studentService.getStudentsByNums(students_num);
-        await studentService.updateStudentClub(to_club_name, students_num);
-        const clubAndName = {};
-        for (let student of students) {
-          const { club_name, name } = student;
-          if (!clubAndName[club_name]) {
-            clubAndName[club_name] = [];
-          }
-          clubAndName[club_name].push(name);
-        }
-        const { id } = res.locals.payload;
-        for (let clubName in clubAndName) {
-          noticeEventEmitter.emit(
-            studentClubChange,
-            clubName,
-            to_club_name,
-            clubAndName[clubName],
-            id
-          );
-        }
-        res.status(200).json();
-      } catch (e) {
-        console.log(e);
-        next(e);
-      }
-    }
+    tryCatchHandler(ClubController.moveStudentsClub)
   );
 
   route.post(
     "/",
     isAuth,
     validate({ schema: addClubSchema, property: Property.BODY }),
-    async (req: Request, res: Response, next: NextFunction) => {
-      const club: IClub = req.body.club;
-      const studentsNum: string[] = req.body.students_num;
-      try {
-        const createdClub = await clubService.addClub(club, studentsNum);
-        const { name, location } = club;
-        const { id } = res.locals.payload;
-        noticeEventEmitter.emit(clubAdd, name, location, id);
-        res.status(200).json(createdClub);
-      } catch (e) {
-        next(e);
-      }
-    }
+    tryCatchHandler(ClubController.createClub)
   );
 
   route.get(
     "/:name",
     isAuth,
     validate({ schema: getClubNameSchema, property: Property.PARAMS }),
-    async (req: Request, res: Response, next: NextFunction) => {
-      const { name } = req.params;
-      try {
-        const clubAndStudents = await clubService.getClubByNameWithStudents(
-          name
-        );
-        res.status(200).json(clubAndStudents);
-      } catch (e) {
-        next(e);
-      }
-    }
+    tryCatchHandler(ClubController.getClubInformation)
   );
 
   route.delete(
     "/:name",
     isAuth,
     validate({ schema: deleteClubSchema, property: Property.PARAMS }),
-    async (req: Request, res: Response, next: NextFunction) => {
-      const { name } = req.params;
-      try {
-        const { location } = await clubService.getClubByName(name);
-        await clubService.deleteClub(name);
-        const { id } = res.locals.payload;
-        noticeEventEmitter.emit(clubDelete, name, location, id);
-        res.status(200).json();
-      } catch (e) {
-        next(e);
-      }
-    }
+    tryCatchHandler(ClubController.deleteClub)
   );
 
   route.patch(
@@ -139,23 +51,6 @@ export default (app: Router) => {
     isAuth,
     validate({ schema: updateClubParamSchema, property: Property.PARAMS }),
     validate({ schema: updateClubBodySchema, property: Property.BODY }),
-    async (req: Request, res: Response, next: NextFunction) => {
-      const { name } = req.params;
-      const clubInfoWillChange: IUpdateClub = req.body;
-      try {
-        const beforeClub = await clubService.getClubByName(name);
-        await clubService.updateClubInformation(name, clubInfoWillChange);
-        const { id } = res.locals.payload;
-        noticeEventEmitter.emit(
-          clubInfoChange,
-          beforeClub,
-          clubInfoWillChange,
-          id
-        );
-        res.status(200).json();
-      } catch (e) {
-        next(e);
-      }
-    }
+    tryCatchHandler(ClubController.updateClubInformation)
   );
 };
