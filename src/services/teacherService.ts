6a4cf47039de1cc24240f3c2a3557b07d7ab4d29
김대웅 <dkssud9556@dsm.hs.kr>
2021-04-04 16:22:19 +0900

    [ADD] teacher service

---
+++ src/services/teacherService.ts
@@ -0,0 +1,9 @@
+import TeacherRepository from "../interfaces/teacher/TeacherRepository";
+
+export default class TeacherService {
+	constructor(private teacherRepository: TeacherRepository) {}
+
+	public findTeachersByName(name: string) {
+		return this.teacherRepository.findTeachersByName(name);
+	}
+}
